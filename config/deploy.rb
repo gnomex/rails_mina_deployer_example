ad26031a3cfc8c3c4be1d6c3a210817871d40017
Kenner Kliemann <kenner.hp@gmail.com>
2014-04-24 10:00:04 -0300

    Adding secrets shared file on mina. Only to rails 4.1.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/unicorn.rb', 'log']
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'config/unicorn.rb', 'log']
 
 # Optional settings:
 set :user, 'deploy'
